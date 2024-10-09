c77203c83e06e99f9a0ccdd1d1e6084afbbdb451
Gabriel Alvise <148637079+Alvisera@users.noreply.github.com>
2024-10-09 18:29:45 -0300

    Update server.py

--- server.py
+++ server.py
@@ -13,15 +13,19 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/telainicial')
+def tela_inicial():
+    return render_template('tela_inicial.html')
+
 @app.route('/chat')
 def chat():
     return render_template('index.html')
 
 @socketio.on('message')
 def handle_message(msg):
-    print(f"Mensagem recebida: {msg}")
-    
+    print(f"Mensagem recebida no servidor: {msg}")
     send(msg, broadcast=True)
 
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5500, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
