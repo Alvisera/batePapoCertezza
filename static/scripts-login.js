2bbbe70ff4208b32ca3062fe845bb5a0ae95945c
Gabriel Alvise <148637079+Alvisera@users.noreply.github.com>
2024-10-09 18:30:57 -0300

    Update scripts-login.js

--- static/scripts-login.js
+++ static/scripts-login.js
@@ -1,9 +1,11 @@
 const form = document.getElementById('loginForm');
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            const username = document.getElementById('username').value;
+form.addEventListener('submit', function(event) {   
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const usernameBeforeAt = username.split('.')[0];
+    const formattedUsername = usernameBeforeAt.charAt(0).toUpperCase() + usernameBeforeAt.slice(1);
 
-            localStorage.setItem('username', username);
+    localStorage.setItem('username', formattedUsername);
 
-            window.location.href = '/chat';
-        });
+    window.location.href = '/telainicial';
+});
